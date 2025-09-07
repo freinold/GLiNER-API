e3859ccacb6bd7abe5beffa93841ab3217fb9fa2
Fabian Reinold <fabian.reinold@protonmail.com>
2025-09-07 20:15:43 +0200

    feat(metrics): support dual stack prometheus server

--- main.py
+++ main.py
@@ -16,15 +16,28 @@ def main() -> None:
     if config.metrics_enabled:
         if config.metrics_port == config.port:
             raise ValueError("Metrics port cannot be the same as API port. Please set a different port for metrics.")
-        prometheus_host = config.host if config.host != "" else "0.0.0.0"
-        metrics_server, metrics_thread = start_http_server(addr=prometheus_host, port=config.metrics_port)
-        logger.info(f"Prometheus metrics server started at http://{prometheus_host}:{config.metrics_port}")
+        if config.host == "":
+            # Dual stack: listen on both IPv4 and IPv6
+            ipv6_metrics_server, ipv6_metrics_thread = start_http_server(addr="::", port=config.metrics_port)
+            ipv4_metrics_server, ipv4_metrics_thread = start_http_server(addr="0.0.0.0", port=config.metrics_port)
+            logger.info(f"Prometheus metrics server started at http://[::]:{config.metrics_port} and http://0.0.0.0:{config.metrics_port}")
 
-        @app.on_event("shutdown")
-        async def close_metrics_server():
-            metrics_server.shutdown()
-            metrics_thread.join()
-            logger.info("Prometheus metrics server shutdown complete.")
+            @app.on_event("shutdown")
+            async def close_dual_metrics_server():
+                ipv6_metrics_server.shutdown()
+                ipv6_metrics_thread.join()
+                ipv4_metrics_server.shutdown()
+                ipv4_metrics_thread.join()
+                logger.info("Prometheus metrics server shutdown complete.")
+        else:
+            metrics_server, metrics_thread = start_http_server(addr=config.host, port=config.metrics_port)
+            logger.info(f"Prometheus metrics server started at http://{config.host}:{config.metrics_port}")
+
+            @app.on_event("shutdown")
+            async def close_metrics_server():
+                metrics_server.shutdown()
+                metrics_thread.join()
+                logger.info("Prometheus metrics server shutdown complete.")
 
     if config.frontend_enabled:
         from fastapi.staticfiles import StaticFiles
